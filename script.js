49d0505626cbfd246773adef397801133ce9677a
Kimtoun07 <148827907+Kimtoun07@users.noreply.github.com>
2025-05-02 15:46:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-<script type="text/javascript">
-	atOptions = {
-		'key' : 'a617ddd5d803efb4a878b4c52d4d9f63',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/a617ddd5d803efb4a878b4c52d4d9f63/invoke.js"></script>
+
